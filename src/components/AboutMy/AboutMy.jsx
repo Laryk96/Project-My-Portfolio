93116f80a7a92fd7ae9db5f451881d9e5288e09c
Lary <110429721+Laryk96@users.noreply.github.com>
2023-03-07 15:21:50 +0200

    Update AboutMy.jsx

--- src/components/AboutMy/AboutMy.jsx
+++ src/components/AboutMy/AboutMy.jsx
@@ -1,24 +1,32 @@
-import './AboutMy.css'
+import React, { useRef } from 'react';
+import { useReactToPrint } from 'react-to-print';
+
+import resume from 'download/Lary_Aguebor_Frontend-Dev.pdf';
+import './AboutMy.css';
+import { Link } from 'react-router-dom';
 
 const AboutMy = () => {
-	return (
-		<section className='aboutMy'>
-			<div className='aboutMy__wrapper'>
-				<h1 className='aboutMy__title'>
-					<strong>
-						Hi, my name is <em>Lary</em>
-					</strong>
-					<br />a frontend developer
-				</h1>
-				<div className='aboutMy__text'>
-					<p>with passion for learning and creating.</p>
-				</div>
-				<a href='#!' className='btn'>
-					Download CV
-				</a>
-			</div>
-		</section>
-	)
-}
+  const componentRef = useRef(resume);
 
-export default AboutMy
+  const handleClick = useReactToPrint({ content: () => componentRef.current });
+  return (
+    <section className="aboutMy">
+      <div className="aboutMy__wrapper">
+        <h1 className="aboutMy__title">
+          <strong>
+            Hi, my name is <em>Lary</em>
+          </strong>
+          <br />a frontend developer
+        </h1>
+        <div className="aboutMy__text">
+          <p>with passion for learning and creating.</p>
+        </div>
+        <Link to={resume} download={resume} className="btn" onClick={handleClick}>
+          Download CV
+        </Link>
+      </div>
+    </section>
+  );
+};
+
+export default AboutMy;
